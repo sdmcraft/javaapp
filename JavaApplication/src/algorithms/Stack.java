802fe448f5605edb4c99e9d4afbaf0c25dce10f3
m.satyadeep <m.satyadeep@c4b4fcbc-a5bb-11dd-8805-4530818397e5>
2009-01-16 13:11:35 +0000

    Migrated to JavaApp

--- JavaApplication/src/algorithms/Stack.java
+++
@@ -1,8 +0,0 @@
-package algorithms;
-
-public interface Stack {
-    public int pop() throws Exception;
-    public void push(int item) throws Exception;
-    public int peek() throws Exception;   
-    public boolean isEmpty();
-}
