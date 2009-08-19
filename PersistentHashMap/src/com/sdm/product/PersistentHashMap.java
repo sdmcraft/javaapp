ac34969bb1f5e0f880bab0d8e7e83db8b552f67f
m.satyadeep <m.satyadeep@c4b4fcbc-a5bb-11dd-8805-4530818397e5>
2009-08-19 06:25:52 +0000

    Added javadocs and modifies size()

--- PersistentHashMap/src/com/sdm/product/PersistentHashMap.java
+++ PersistentHashMap/src/com/sdm/product/PersistentHashMap.java
@@ -9,7 +9,6 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
@@ -55,6 +54,18 @@ public class PersistentHashMap<K, V> implements Serializable {
 	private File currentMapFile = null;
 	private boolean currentMapChanged = false;
 
+	/**
+	 * Constructs an emptry PersistentHashMap with the specified backup
+	 * directory and maximum entries
+	 * 
+	 * @param backupDir
+	 *            The directory to place backing files
+	 * @param maxEntries
+	 *            Maximum number of entries retained in memory before being
+	 *            written to backing files
+	 * @throws Exception
+	 *             If an internal error occurred
+	 */
 	public PersistentHashMap(File backupDir, long maxEntries) throws Exception {
 		logger.debug("(+)PersistentHashMap(+)");
 		logger.debug("Args:backupDir->" + backupDir);
@@ -67,6 +78,21 @@ public class PersistentHashMap<K, V> implements Serializable {
 		logger.debug("(-)PersistentHashMap(-)");
 	}
 
+	/**
+	 * Constructs an PersistentHashMap from the specified hashmap with the
+	 * specified backup directory and maximum entries
+	 * 
+	 * @param map
+	 *            The hashmap whose entries will be put in this
+	 *            PersistentHashMap
+	 * @param backupDir
+	 *            The directory to place backing files
+	 * @param maxEntries
+	 *            Maximum number of entries retained in memory before being
+	 *            written to backing files
+	 * @throws Exception
+	 *             If an internal error occurred
+	 */
 	public PersistentHashMap(HashMap<K, V> map, File backupDir, long maxEntries)
 			throws Exception {
 		logger.debug("(+)PersistentHashMap(+)");
@@ -112,6 +138,25 @@ public class PersistentHashMap<K, V> implements Serializable {
 		logger.debug("(-)createCurrentMap(-)");
 	}
 
+	/**
+	 * Returns the value to which the specified key is mapped in this identity
+	 * hash map, or null if the map contains no mapping for this key. A return
+	 * value of null does not necessarily indicate that the map contains no
+	 * mapping for the key; it is also possible that the map explicitly maps the
+	 * key to null. The containsKey method may be used to distinguish these two
+	 * cases.
+	 * <p>
+	 * As a side effect, this method causes the entries in memory to be written
+	 * to the backing files and be replaced by another set on entries.
+	 * </p>
+	 * 
+	 * @param key
+	 *            the key whose associated value is to be returned.
+	 * @return the value to which this map maps the specified key, or null if
+	 *         the map contains no mapping for this key.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public V get(Object key) throws Exception {
 		logger.debug("(+)get(+)");
 		logger.debug("Args:key->" + key);
@@ -211,6 +256,21 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return result;
 	}
 
+	/**
+	 * Associates the specified value with the specified key in this map. If the
+	 * map previously contained a mapping for this key, the old value is
+	 * replaced.
+	 * 
+	 * @param key
+	 *            key with which the specified value is to be associated.
+	 * @param value
+	 *            value to be associated with the specified key.
+	 * @return previous value associated with specified key, or null if there
+	 *         was no mapping for key. A null return can also indicate that the
+	 *         HashMap previously associated null with the specified key.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public V put(K key, V value) throws Exception {
 		logger.debug("(+)put(+)");
 		logger.debug("Args:key->" + key);
@@ -234,6 +294,17 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return returnValue;
 	}
 
+	/**
+	 * Returns a string representation of this map. The string representation
+	 * consists of a list of key-value mappings in the order returned by the
+	 * map's entrySet view's iterator, enclosed in braces ("{}"). Adjacent
+	 * mappings are separated by the characters ", " (comma and space). Each
+	 * key-value mapping is rendered as the key followed by an equals sign ("=")
+	 * followed by the associated value.
+	 * 
+	 * @return a String representation of this map.
+	 */
+	@Override
 	public String toString() {
 		logger.debug("(+)toString(+)");
 		String prefix = "{";
@@ -274,63 +345,56 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return string;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((backupDir == null) ? 0 : backupDir.hashCode());
-		result = prime * result
-				+ ((backupFilesMap == null) ? 0 : backupFilesMap.hashCode());
-		result = prime * result
-				+ (int) (currentEntries ^ (currentEntries >>> 32));
-		result = prime * result
-				+ ((currentMap == null) ? 0 : currentMap.hashCode());
-		result = prime * result + (currentMapChanged ? 1231 : 1237);
-		result = prime * result
-				+ ((currentMapFile == null) ? 0 : currentMapFile.hashCode());
-		result = prime * result + (int) (maxEntries ^ (maxEntries >>> 32));
-		return result;
-	}
+	// @Override
+	// public int hashCode() {
+	// final int prime = 31;
+	// int result = 1;
+	// result = prime * result
+	// + ((backupDir == null) ? 0 : backupDir.hashCode());
+	// result = prime * result
+	// + ((backupFilesMap == null) ? 0 : backupFilesMap.hashCode());
+	// result = prime * result
+	// + (int) (currentEntries ^ (currentEntries >>> 32));
+	// result = prime * result
+	// + ((currentMap == null) ? 0 : currentMap.hashCode());
+	// result = prime * result + (currentMapChanged ? 1231 : 1237);
+	// result = prime * result
+	// + ((currentMapFile == null) ? 0 : currentMapFile.hashCode());
+	// result = prime * result + (int) (maxEntries ^ (maxEntries >>> 32));
+	// return result;
+	// }
+	//
+	// @Override
+	// public boolean equals(Object obj) {
+	// if (this == obj)
+	// return true;
+	// if (obj == null)
+	// return false;
+	// if (getClass() != obj.getClass()
+	// && !HashMap.class.equals(obj.getClass()))
+	// return false;
+	// try {
+	// if (HashMap.class.equals(obj.getClass())) {
+	// if (!toHashMap().equals(obj))
+	// return false;
+	// } else if (!toHashMap().equals(
+	// ((PersistentHashMap<K,V>) obj).toHashMap()))
+	// return false;
+	// } catch (Exception ex) {
+	// throw new RuntimeException(ex);
+	// }
+	// return true;
+	// }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final PersistentHashMap other = (PersistentHashMap) obj;
-		if (backupDir == null) {
-			if (other.backupDir != null)
-				return false;
-		} else if (!backupDir.equals(other.backupDir))
-			return false;
-		if (backupFilesMap == null) {
-			if (other.backupFilesMap != null)
-				return false;
-		} else if (!backupFilesMap.equals(other.backupFilesMap))
-			return false;
-		if (currentEntries != other.currentEntries)
-			return false;
-		if (currentMap == null) {
-			if (other.currentMap != null)
-				return false;
-		} else if (!currentMap.equals(other.currentMap))
-			return false;
-		if (currentMapChanged != other.currentMapChanged)
-			return false;
-		if (currentMapFile == null) {
-			if (other.currentMapFile != null)
-				return false;
-		} else if (!currentMapFile.equals(other.currentMapFile))
-			return false;
-		if (maxEntries != other.maxEntries)
-			return false;
-		return true;
-	}
-
+	/**
+	 * Returns a HashMap representation of this PersistentHashMap. Since a
+	 * PersistentHashMap can be potentially bigger than the system memory size,
+	 * there is a risk of running out of heap space by calling this method
+	 * 
+	 * @return HashMap representation of this PersistentHashMap
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public HashMap<K, V> toHashMap() throws Exception {
 		logger.debug("(+)toMap(+)");
 		Set<Entry<File, Long>> entrySet = backupFilesMap.entrySet();
@@ -348,6 +412,15 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return resultMap;
 	}
 
+	/**
+	 * Returns true if this map contains a mapping for the specified key
+	 * 
+	 * @param key
+	 *            key whose presence in this map is to be tested.
+	 * @return true if this map contains a mapping for the specified key
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public boolean containsKey(Object key) throws Exception {
 		logger.debug("(+)containsKey(+)");
 		boolean result = false;
@@ -368,6 +441,17 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return result;
 	}
 
+	/**
+	 * Returns true if this map maps one or more keys to this value. More
+	 * formally, returns true if and only if this map contains at least one
+	 * mapping to a value v such that (value==null ? v==null : value.equals(v)).
+	 * 
+	 * @param value
+	 *            value whose presence in this map is to be tested.
+	 * @return true if this map maps one or more keys to this value.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public boolean containsValue(Object value) throws Exception {
 		logger.debug("(+)containsValue(+)");
 		boolean result = false;
@@ -388,22 +472,29 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return result;
 	}
 
-	private Set<Map.Entry<K, V>> entrySet() throws Exception {
-		logger.debug("(+)entrySet(+)");
-		Set<Map.Entry<K, V>> resultSet = new HashSet<Map.Entry<K, V>>();
-		resultSet.addAll(currentMap.entrySet());
-		Set<Entry<File, Long>> entrySet = backupFilesMap.entrySet();
-		for (Entry<File, Long> entry : entrySet) {
-			if ((currentMapFile != null && currentMapFile.exists() && !entry
-					.getKey().getAbsolutePath().equals(
-							currentMapFile.getAbsolutePath()))
-					|| (currentMapFile == null))
-				resultSet.addAll(readMap(entry.getKey()).entrySet());
-		}
-		logger.debug("(-)entrySet(-)");
-		return resultSet;
-	}
+	// private Set<Map.Entry<K, V>> entrySet() throws Exception {
+	// logger.debug("(+)entrySet(+)");
+	// Set<Map.Entry<K, V>> resultSet = new HashSet<Map.Entry<K, V>>();
+	// resultSet.addAll(currentMap.entrySet());
+	// Set<Entry<File, Long>> entrySet = backupFilesMap.entrySet();
+	// for (Entry<File, Long> entry : entrySet) {
+	// if ((currentMapFile != null && currentMapFile.exists() && !entry
+	// .getKey().getAbsolutePath().equals(
+	// currentMapFile.getAbsolutePath()))
+	// || (currentMapFile == null))
+	// resultSet.addAll(readMap(entry.getKey()).entrySet());
+	// }
+	// logger.debug("(-)entrySet(-)");
+	// return resultSet;
+	//	}
 
+	/**
+	 * Returns true if this map contains no key-value mappings.
+	 * 
+	 * @return true if this map contains no key-value mappings.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public boolean isEmpty() throws Exception {
 		logger.debug("(+)isEmpty(+)");
 		boolean empty = true;
@@ -426,6 +517,16 @@ public class PersistentHashMap<K, V> implements Serializable {
 		return empty;
 	}
 
+	/**
+	 * Copies all of the mappings from the specified map to this map (optional
+	 * operation). These mappings will replace any mappings that this map had
+	 * for any of the keys currently in the specified map.
+	 * 
+	 * @param map
+	 *            mappings to be stored in this map.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public void putAll(Map map) throws Exception {
 		logger.debug("(+)putAll(+)");
 		if (map == null)
@@ -438,9 +539,18 @@ public class PersistentHashMap<K, V> implements Serializable {
 		logger.debug("(-)putAll(-)");
 	}
 
+	/**
+	 * Returns the number of key-value mappings in this map. If the map contains
+	 * more than Integer.MAX_VALUE elements, returns Integer.MAX_VALUE.
+	 * 
+	 * @return the number of key-value mappings in this map.
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public int size() throws Exception {
 		logger.debug("(+)size(+)");
-		int size = currentMap.size();
+		int returnSize;
+		long size = currentMap.size();
 		Set<Entry<File, Long>> entrySet = backupFilesMap.entrySet();
 		for (Entry<File, Long> entry : entrySet) {
 			if ((currentMapFile != null && currentMapFile.exists() && !entry
@@ -449,10 +559,27 @@ public class PersistentHashMap<K, V> implements Serializable {
 					|| (currentMapFile == null))
 				size += readMap(entry.getKey()).size();
 		}
+		if (size > Integer.MAX_VALUE)
+			returnSize = Integer.MAX_VALUE;
+		else
+			returnSize = (int) size;
 		logger.debug("(-)size(-)");
-		return size;
+		return returnSize;
 	}
 
+	/**
+	 * Removes the mapping for this key from this map if present (optional
+	 * operation).
+	 * 
+	 * @param key
+	 *            key whose mapping is to be removed from the map.
+	 * @return previous value associated with specified key, or null if there
+	 *         was no entry for key. (A null return can also indicate that the
+	 *         map previously associated null with the specified key, if the
+	 *         implementation supports null values.)
+	 * @throws Exception
+	 *             if an internal error occurred
+	 */
 	public V remove(Object key) throws Exception {
 		logger.debug("(+)remove(+)");
 		logger.debug("Arg:key->" + key);
