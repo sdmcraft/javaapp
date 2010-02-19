90456aae30a7823fb8ef513f16b1a54cb0955c8e
m.satyadeep <m.satyadeep@c4b4fcbc-a5bb-11dd-8805-4530818397e5>
2010-02-19 10:29:40 +0000

    Legacy

--- FLVParser/src/com/sdm/FLVParser/datatypes/AMFAnonymousObject.java
+++
@@ -1,6 +0,0 @@
-package com.sdm.FLVParser.datatypes;
-
-public class AMFAnonymousObject //extends AMFValue
-{	
-	private ObjectProperty[] value;
-}
--- FLVParser/src/com/sdm/FLVParser/datatypes/TagData.java
+++
@@ -1,7 +0,0 @@
-package com.sdm.FLVParser.datatypes;
-
-import java.io.OutputStream;
-
-public interface TagData {
-	public void write(OutputStream out) throws Exception;
-}
